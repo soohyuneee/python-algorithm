a4a9a4212426ef5ec964846bf7222a760a0eeff6
soohyuneee <tlstngus95@naver.com>
2023-07-19 23:31:48 +0900

    [level 0] Title: 각도기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120829. 각도기/각도기.py
+++ 프로그래머스/lv0/120829. 각도기/각도기.py
@@ -1,9 +1,9 @@
 def solution(angle):
     if angle == 180:
         answer =  4
-    elif 90 < angle : 
+    elif 90 < angle: 
         answer = 3
-    elif angle == 90 : 
+    elif angle == 90: 
         answer = 2
     else:
         answer = 1
