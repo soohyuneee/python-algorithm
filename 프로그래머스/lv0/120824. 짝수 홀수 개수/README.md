e0d6adb27b0f3de023f9adc3b1631e246c4bc8d5
soohyuneee <tlstngus95@naver.com>
2023-07-31 17:45:16 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/README.md
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/README.md
@@ -1,6 +1,6 @@
 # [level 0] 짝수 홀수 개수 - 120824 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120824?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120824) 
 
 ### 성능 요약
 
--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -1,8 +1,5 @@
 def solution(num_list):
-    even, odd = 0, 0
+    answer = [0,0]
     for n in num_list:
-        if n % 2 == 0 :
-            even += 1
-        else :
-            odd += 1
-    return [even, odd]
+        answer[n%2] += 1
+    return answer
